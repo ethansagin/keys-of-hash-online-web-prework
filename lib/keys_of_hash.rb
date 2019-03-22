cd28c1cf0e9db1f7e4b0de482b9f2c3bec60a403
Ethan <ethansagin@gmail.com>
2019-03-22 22:15:04 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,6 +2,13 @@ require 'pry'
 
 class Hash
   def keys_of(*args)
-    array = []
+    map do |k, v|
+      if args.include?(v)
+        k
+      else
+        nil
+      end
+    end
+    
   end
 end
