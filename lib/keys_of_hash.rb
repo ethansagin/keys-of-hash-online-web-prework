7b375407d5e86705705ac8760d53ffacaa64ae88
Ethan <ethansagin@gmail.com>
2019-03-22 22:20:11 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*args)
+    self.map { |k, v|
+      if args.include?(v)
+        k
+      else
+        nil
+      end
+    }.compact
   end
 end
